0ee5d9181c7a22f6f6a0db9849138f432a27a64b
Juan Pablo Alfonso Sánchez <69476217+JPalfonsos@users.noreply.github.com>
2023-08-06 19:35:54 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,17 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import buildspaceLogo from '../assets/buildspace-logo.png';
+import { useState } from 'react';
+
 
 
 const Home = () => {
+  const [userInput, setUserInput] = useState('');
+  
+  const onUserChangedText = (event) => {
+  console.log(event.target.value);
+  setUserInput(event.target.value);
+};
   return (
     <div className="root">
       <div className="container">
@@ -15,7 +23,12 @@ const Home = () => {
             <h2>insert your subtitle here</h2>
           </div>
         </div>
-        {/* Add this code here*/}
+        <textarea
+  className="prompt-box"
+  placeholder="start typing here"
+  value={userInput}
+  onChange={onUserChangedText}
+/>;
         <div className="prompt-container">
           <textarea placeholder="start typing here" className="prompt-box" />
         </div>
